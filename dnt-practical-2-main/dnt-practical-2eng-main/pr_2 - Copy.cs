3351c96870294d2fa0391e7628afa046848e8af4
yjpatel0 <61009655+yjpatel0@users.noreply.github.com>
2022-04-27 11:43:00 +0530

    Add files via upload

---
+++ dnt-practical-2-main/dnt-practical-2eng-main/pr_2 - Copy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Km, Wg, Charge;
+            Console.WriteLine("Enter the value of Km");
+            Km = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the value of Wg");
+            Wg = int.Parse(Console.ReadLine());
+            if (Km <= 100)
+            {
+                Charge = 50 * Wg;
+            }
+            else if (Km >100 && Km <= 200)
+            {
+                Charge = 65 * Wg;
+            }
+            else if (Km > 200 && Km <= 300)
+            {
+                Charge = 90 * Wg;
+            }
+            else
+            {
+                Charge = 120 * Wg;
+            }
+            Console.WriteLine("Charge =" + Charge);
+            Console.ReadLine();
+
+        }
+    }
+}
